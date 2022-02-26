18c74fb08ad6d3a6d6282793b94373a9479ebd59
semacedonio <semacedonio@gmail.com>
2022-02-26 13:12:19 -0600

    Omitted Privacy View and Link

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -73,10 +73,10 @@ namespace MacLibrary.Controllers
                 return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
