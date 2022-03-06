8438236c59a76721745e3dda1d1f357354eb022f
semacedonio <semacedonio@gmail.com>
2022-03-06 17:22:16 -0600

    Simplified 'new' expression

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -92,7 +92,7 @@ namespace MacLibrary.Controllers
                 //6.1.4 Working with parameters
                 string sql = "Select Name, HeadOfState From Country Where Continent = @Continent";
 
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlCommand cmd = new (sql, conn);
                 Console.WriteLine("Enter a continent e.g. 'Europe', 'North America' etc.:");
                 string user_input = Console.ReadLine();
                 cmd.Parameters.AddWithValue("@Continent", "North America");
